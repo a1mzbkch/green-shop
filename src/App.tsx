86ef49bc721cad1cea488342dbda204a7d4be85d
bakdoolot <nurisulajmanov.com>
2025-09-06 16:54:07 +0600

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import axios from "axios";
 import "./App.scss";
+import { useEffect } from "react";
+import Header from "./components/layout/header/Header";
 
 function App() {
-  return (
-    <div className="app">
-      <></>
-    </div>
-  );
+  async function getProduct() {
+    let res = await axios(
+      `https://green-shop-backend-1.onrender.com/products/`
+    );
+    console.log(res.data);
+  }
+  useEffect(() => {
+    getProduct()
+  },[])
+
+  return <div className="app">
+    <Header/>
+  </div>;
 }
 
 export default App;

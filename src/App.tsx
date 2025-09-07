0c403d4e0a41bd297547fc8292cca6142f825f48
bakdoolot <nurisulajmanov.com>
2025-09-07 15:56:59 +0600

    productCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import axios from "axios";
 import "./App.scss";
-import { useEffect } from "react";
 import Header from "./components/layout/header/Header";
+import MainPage from "./components/main/MainPage";
 
 function App() {
-  async function getProduct() {
-    let res = await axios(
-      `https://green-shop-backend-1.onrender.com/products/`
-    );
-    console.log(res.data);
-  }
-  useEffect(() => {
-    getProduct()
-  },[])
-
-  return <div className="app">
-    <Header/>
-  </div>;
+  return (
+    <div className="app">
+      <Header />
+      <MainPage/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/main/MainPage.tsx
@@ -0,0 +1,15 @@
+import Product from './product/Product';
+
+const MainPage = () => {
+  return (
+    <section id='mainPage'>
+      <div className="container">
+        <div className="mainPage">
+          <Product/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainPage;

726d9c38a4f3ea1b7ca7ce2256a23896b9b9483b
osorbecovk <osorbecov@gmail.com>
2025-09-07 15:46:48 +0600

    Fix

--- src/main.js
+++ src/main.js
@@ -4,4 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  _jsx(React.StrictMode, {
+    children: _jsx(BrowserRouter, { children: _jsx(App, {}) }),
+  })
+);
